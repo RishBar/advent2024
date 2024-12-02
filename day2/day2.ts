7e7d418aee8e89313b3989b487bfdf353b7ed961
RishBar <rishav@baral.ca>
2024-12-02 15:44:30 -0800

    day 2 part 1

---
+++ day2/day2.ts
@@ -0,0 +1,50 @@
+// PART 1
+
+/*
+*/
+
+import { day2Input } from './day2Input'
+
+
+const formatRawInput = (rawInput: string): number[][] => {
+    return rawInput.split('\n').map(line => line.split(' ').map(value => Number(value)))
+}
+
+const findSafeReports = (reports: number[][]): number => {
+    const safeReports = reports.filter((report) => {
+        let isSafe = true;
+
+        const isLevelAscending = (report[1] - report[0] > 0) ? true : false;
+
+        for (let index = 0; index < report.length; index++) {
+            if (index === 0) {
+                continue;
+            }
+
+            const levelSpike = report[index] - report[index - 1];
+            const absoluteLevelSpike = Math.abs(levelSpike);
+
+            if (absoluteLevelSpike === 0 || absoluteLevelSpike > 3) {
+                isSafe = false;
+                break;
+            }
+
+            if (isLevelAscending && levelSpike < 0) {
+                isSafe = false;
+                break;
+            }
+
+            if (!isLevelAscending && levelSpike > 0) {
+                isSafe = false;
+                break;
+            }
+        }
+
+        return isSafe;
+    })
+
+    return safeReports.length;
+}
+
+const input = formatRawInput(day2Input)
+console.log(findSafeReports(input))
