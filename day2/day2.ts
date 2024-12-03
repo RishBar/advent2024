66647e048313d07790cef8c124fde8b00cdda544
RishBar <rishav@baral.ca>
2024-12-03 13:53:28 -0800

    brute force solution for day 2 part 2

--- day2/day2.ts
+++ day2/day2.ts
@@ -1,8 +1,5 @@
 // PART 1
 
-/*
-*/
-
 import { day2Input } from './day2Input'
 
 
@@ -44,3 +41,46 @@ const findSafeReports = (reports: number[][]): number => {
 
 const input = formatRawInput(day2Input)
 console.log(findSafeReports(input))
+
+
+// PART 2
+
+const bruteForce = (reports: number[][]): number => {
+    const safeReports = reports.filter((report) => {        
+        let isSafe = false;
+        for (let elementToRemove = -1; elementToRemove < report.length; elementToRemove++) {
+            const adjustedReport = report.filter((_, index) => elementToRemove !== index)
+
+            const isLevelAscending = (adjustedReport[1] - adjustedReport[0] > 0) ? true : false;
+
+            for (let index = 1; index < adjustedReport.length; index++) {
+                const levelSpike = adjustedReport[index] - adjustedReport[index - 1];
+                const absoluteLevelSpike = Math.abs(levelSpike);
+
+                if (absoluteLevelSpike === 0 || absoluteLevelSpike > 3) {
+                    break;
+                }
+
+                if (isLevelAscending && levelSpike < 0) {
+                    break;
+                }
+
+                if (!isLevelAscending && levelSpike > 0) {
+                    break;
+                }
+
+                if (index === adjustedReport.length - 1) {
+                    isSafe = true;
+                }
+            }
+
+            if (isSafe) {
+                break;
+            }
+        }
+
+        return isSafe;
+    })
+
+    return safeReports.length;
+}
